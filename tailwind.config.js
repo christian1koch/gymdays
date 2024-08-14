0784c32005f2e0d6c6fd702c0364ef31ddb526e7
Christian Koch Echeverria <ko.che@web.de>
2024-08-15 00:40:40 +0200

    fix tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,jsx,ts,tsx}",
-    "./checklist-page/**/*.{js,jsx,ts,tsx}",
-    "./ui/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: [
+		"./app/**/*.{js,jsx,ts,tsx}",
+		"./checklist-page/**/*.{js,jsx,ts,tsx}",
+		"./libs/**/*.{js,jsx,ts,tsx}",
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
 };
