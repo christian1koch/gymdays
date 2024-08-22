855fc56cc988e981c00469ea4cd0db8e4e744cf0
Christian Koch Echeverria <ko.che@web.de>
2024-08-23 00:44:34 +0200

    #33 disable drizzle in prod

--- gymdays-frontend/libs/gymdays/features/hooks/useDrizzleStudioWithDb.ts
+++ gymdays-frontend/libs/gymdays/features/hooks/useDrizzleStudioWithDb.ts
@@ -1,6 +1,6 @@
 import { getDBSync, initDatabase } from "@gymDays/db";
 import { useDrizzleStudio } from "expo-drizzle-studio-plugin";
 export default function useDrizzleStudioWithDB() {
-	const database = getDBSync();
-	useDrizzleStudio(database);
+	// const database = getDBSync();
+	// useDrizzleStudio(database);
 }
