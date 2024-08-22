c38fd252229d92a36cea8fc0bc31b366175e1684
Christian Koch Echeverria <ko.che@web.de>
2024-08-23 00:13:13 +0200

    #33 improve dev

--- gymdays-backend/db.js
+++ gymdays-backend/db.js
@@ -1,21 +1,31 @@
 import { sql } from "@vercel/postgres";
 
+const isDevMode = () => {
+    if (!process.env.DEV_MODE) {
+        return false;
+    }
+    if (process.env.DEV_MODE.toLocaleLowerCase() === "false") {
+        return false;
+    }
+    return true;
+};
+
 export async function createBackup(userId = "", link = "") {
-    if (process.env.DEV_MODE) {
+    if (isDevMode()) {
         return await sql`INSERT INTO backup_dev (user_Id, backup_data) VALUES (${userId}, ${link})`;
     }
     return await sql`INSERT INTO backup_prod (user_Id, backup_data) VALUES (${userId}, ${link})`;
 }
 
 export async function replaceBackup(userId = "", link = "") {
-    if (process.env.DEV_MODE) {
+    if (isDevMode()) {
         return await sql`UPDATE backup_dev SET backup_data = ${link} WHERE user_id = ${userId}`;
     }
     return await sql`UPDATE backup_prod SET backup_data = ${link} WHERE user_id = ${userId}`;
 }
 
 export async function getBackupURL(userId = "") {
-    if (process.env.DEV_MODE) {
+    if (isDevMode()) {
         return await sql`SELECT backup_data FROM backup_dev WHERE user_id = ${userId}`;
     }
     return await sql`SELECT backup_data FROM backup_prod WHERE user_id = ${userId}`;
