c5a575badde39c163176d41dafc53d8f21be9377
Christian Koch Echeverria <ko.che@web.de>
2024-08-23 00:51:44 +0200

    #33 addedcorrect prod dev logic

--- gymdays-frontend/libs/gymdays/features/db/backup.ts
+++ gymdays-frontend/libs/gymdays/features/db/backup.ts
@@ -1,10 +1,10 @@
 import * as FileSystem from "expo-file-system";
 import * as db from "@gymDays/db";
+const dbName = __DEV__
+	? process.env.EXPO_PUBLIC_DEV_DB_NAME
+	: process.env.EXPO_PUBLIC_PROD_DB_NAME;
 export function getDBPath() {
-	const pathToDb =
-		FileSystem.documentDirectory +
-		"SQLite/" +
-		process.env.EXPO_PUBLIC_DEV_DB_NAME;
+	const pathToDb = FileSystem.documentDirectory + "SQLite/" + dbName;
 	return pathToDb;
 }
 async function getFileFromPath() {
@@ -12,10 +12,10 @@ async function getFileFromPath() {
 	const pathToDb = getDBPath();
 	const fileRes = await fetch(pathToDb);
 	const blob = await fileRes.blob();
-	if (!process.env.EXPO_PUBLIC_DEV_DB_NAME) {
+	if (!dbName) {
 		throw new Error("No DB Found");
 	}
-	const file = new File([blob], process.env.EXPO_PUBLIC_DEV_DB_NAME, {
+	const file = new File([blob], dbName, {
 		type: "application/x-sqlite3",
 	});
 	return file;
@@ -23,6 +23,10 @@ async function getFileFromPath() {
 interface BackupResponse {
 	id: string;
 }
+
+const BASE_URL = __DEV__
+	? process.env.EXPO_PUBLIC_DEV_API_URL
+	: process.env.EXPO_PUBLIC_PROD_API_URL;
 export async function createBackup() {
 	const pathToDb = getDBPath();
 	const file = await getFileFromPath();
@@ -32,7 +36,7 @@ export async function createBackup() {
 		type: "application/x-sqlite3",
 		uri: pathToDb,
 	} as any);
-	const response = await fetch("http://localhost:8081/backup", {
+	const response = await fetch(BASE_URL!, {
 		method: "POST",
 		body: formData,
 	});
@@ -49,7 +53,7 @@ export async function updateBackup(backupId?: string) {
 		type: "application/x-sqlite3",
 		uri: pathToDb,
 	} as any);
-	const tempRes = await fetch(`http://localhost:8081/backup/${backupId}`, {
+	const tempRes = await fetch(`${BASE_URL}/${backupId}`, {
 		method: "PUT",
 		body: formData,
 	});
@@ -63,7 +67,7 @@ export async function updateBackup(backupId?: string) {
 
 export async function restoreBackup(userId: string) {
 	const pathToDb = getDBPath();
-	const res = await fetch(`http://localhost:8081/backup/${userId}`);
+	const res = await fetch(`${BASE_URL}/${userId}`);
 	const res2 = await res.json();
 	await FileSystem.deleteAsync(pathToDb);
 	await FileSystem.deleteAsync(pathToDb + "-shm");
