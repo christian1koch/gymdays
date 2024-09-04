3d764a30f3b743eb1a9fbcaa3cfa38dd61a1834b
Christian Koch Echeverria <ko.che@web.de>
2024-09-04 22:11:46 +0200

    improve swappable

--- gymdays-frontend/libs/ui/swappable.tsx
+++ gymdays-frontend/libs/ui/swappable.tsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import { ScrollView, View, Text, Animated, PanResponder } from "react-native";
 import { Button } from "tamagui";
 
@@ -7,29 +8,37 @@ interface SwappableProps {
 }
 
 export function Swappable({ children, rightElement }: SwappableProps) {
-	const translateX = new Animated.Value(0);
-	const panResponder = PanResponder.create({
-		onStartShouldSetPanResponder: () => true,
-		onMoveShouldSetPanResponder: () => true,
-		onPanResponderMove: (e, gestureState) => {
-			if (gestureState.dx < 0) {
-				translateX.setValue(gestureState.dx);
-			}
-		},
-		onPanResponderRelease(e, gestureState) {
-			if (gestureState.dx < -50) {
-				Animated.spring(translateX, {
-					toValue: -100,
-					useNativeDriver: true,
-				}).start();
-			} else {
-				Animated.spring(translateX, {
-					toValue: 0,
-					useNativeDriver: true,
-				}).start();
-			}
-		},
-	});
+	const translateX = useRef(new Animated.Value(0)).current;
+	const translateValue = useRef(0);
+	translateX.addListener(({ value }) => (translateValue.current = value));
+	const panResponder = useRef(
+		PanResponder.create({
+			onStartShouldSetPanResponder: () => true,
+			onMoveShouldSetPanResponder: () => true,
+			onPanResponderGrant: (evt, gestureState) => {
+				translateX.setOffset(translateValue.current);
+				translateX.setValue(0);
+			},
+			onPanResponderMove: Animated.event([null, { dx: translateX }]),
+			onPanResponderRelease(e, gestureState) {
+				if (translateValue.current < -50) {
+					translateX.setOffset(0);
+					translateX.setValue(translateValue.current);
+					Animated.spring(translateX, {
+						toValue: -100,
+						useNativeDriver: true,
+					}).start();
+				} else {
+					translateX.setOffset(0);
+					translateX.setValue(translateValue.current);
+					Animated.spring(translateX, {
+						toValue: 0,
+						useNativeDriver: true,
+					}).start();
+				}
+			},
+		})
+	).current;
 	return (
 		<View className="flex-row">
 			<Animated.View
